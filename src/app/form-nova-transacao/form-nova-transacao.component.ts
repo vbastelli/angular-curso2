e0705d032db58e3dc1d6959adb267c1013379e10
Victório Bastelli <victorio.bastelli@sintel.com.br>
2026-01-19 15:11:06 -0300

    aula 3

--- src/app/form-nova-transacao/form-nova-transacao.component.ts
+++ src/app/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.component.html',
-  styleUrl: './form-nova-transacao.component.css'
+  styleUrl: './form-nova-transacao.component.css',
 })
 export class FormNovaTransacaoComponent {
+  valorTransacao = '';
+  tipoTransacao = '';
+  aoSubmeter() {
+    console.log(this.valorTransacao);
+    console.log(this.tipoTransacao);
 
+  }
 }
