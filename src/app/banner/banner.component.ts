a7d075e995c5b72299593bae94f654dfac0abf84
Victório Bastelli <victorio.bastelli@sintel.com.br>
2026-01-19 14:13:30 -0300

    first commit

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ContaComponent } from "./conta/conta.component";
+import { BoasVindasComponent } from "./boas-vindas/boas-vindas.component";
+
+@Component({
+  selector: 'app-banner',
+  imports: [ContaComponent, BoasVindasComponent],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.css'
+})
+export class BannerComponent {
+}
