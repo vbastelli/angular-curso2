7522e135befaea65e769fd9fd0d4cb1eefe98c41
Victório Bastelli <victorio.bastelli@sintel.com.br>
2026-01-27 15:23:27 -0300

    aula 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
 import { FormNovaTransacaoComponent } from './form-nova-transacao/form-nova-transacao.component';
 import { TipoTransacao, Transacao } from './modelos/transacao';
+import { ExtratoComponent } from "./extrato/extrato.component";
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent, FormNovaTransacaoComponent],
+  imports: [BannerComponent, FormNovaTransacaoComponent, ExtratoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { TransacaoComponent } from "./transacao/transacao.component";
+import { Transacao } from '../modelos/transacao';
+
+@Component({
+  selector: 'app-extrato',
+  imports: [TransacaoComponent],
+  templateUrl: './extrato.component.html',
+  styleUrl: './extrato.component.css'
+})
+export class ExtratoComponent {
+  transacoes = input.required<Transacao[]>()
+}
